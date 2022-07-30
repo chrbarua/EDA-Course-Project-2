fde3da2e9bccf95518caa3ac0e3f49f816b0eb26
chrbarua <90236578+chrbarua@users.noreply.github.com>
2022-07-29 20:50:27 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 png(file="C:/Users/prb/Documents/R Programming/exdata_data_NEI_data/plot2.png", width=720, height=480)
 
-plot_2 <- subset(dfc, fips == "24510", c("Emissions", "Year","type"))
+plot_2 <- subset(dfc, fips == "24510", c("Emissions", "year","type"))
 plot_2 <- aggregate(Emissions ~ year, plot_2, sum)
 plot(plot_2$year,plot_2$Emissions, main="Total Baltimore PM2.5 Emissions", "b", xlab="Year", ylab="Emissions (thousand tons)",xaxt="n")
 axis(side=1, at=c("1999", "2002", "2005", "2008"))
