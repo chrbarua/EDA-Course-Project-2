c7f133799bd756d7d450feca90ec15d9c447e2bd
chrbarua <90236578+chrbarua@users.noreply.github.com>
2022-07-29 20:38:24 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 plot_1 <- aggregate(Emissions ~ year, dfc, sum)
 
-plot(plot_1$Year,plot_1$Emissions, main="Total US PM2.5 Emissions", "b", xlab="Year", ylab="Emissions (thousands of tons)",xaxt="n")
+plot(plot_1$year,plot_1$Emissions, main="Total US PM2.5 Emissions", "b", xlab="Year", ylab="Emissions (thousands of tons)",xaxt="n")
 axis(side=1, at=c("1999", "2002", "2005", "2008"))
 
 # Question: Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
