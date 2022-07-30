bf801c7db46a699295f59f01fa6282a6bd36953d
chrbarua <90236578+chrbarua@users.noreply.github.com>
2022-07-29 21:05:37 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,6 +1,6 @@
 png(file="C:/Users/prb/Documents/R Programming/exdata_data_NEI_data/plot4.png", width=720, height=480)
 
-plot_4 <- subset(dfc, grepl('Coal',NEI_SCC$Short.Name, fixed=TRUE), c("Emissions", "year","type", "Short.Name"))
+plot_4 <- subset(dfc, grepl('Coal', dfc$Short.Name, fixed=TRUE), c("Emissions", "year","type", "Short.Name"))
 plot_4 <- aggregate(Emissions ~ year, plot_4, sum)
 
 ggplot(data=plot_4, aes(x=year, y=Emissions)) + geom_line() + geom_point( size=4, shape=21, fill="white") + xlab("Year") + ylab("Emissions (thousands of tons)") + ggtitle("Total United States PM2.5 Coal Emissions")
